bf25ab972f99a65f33eabc0cfaa00d798d7c2260
Robert Forillo <infocord2000@yahoo.com>
2012-09-25 22:04:26 -0700

    lets get it in synch

--- Myinteractive3.rb
+++
@@ -1,8 +0,0 @@
-print "What is your name: "
-STDOUT.flush
-name = gets.chop
-puts "Your name is #{name}"
-puts "Changing all lowercase o's to uppercase O's"
-name.gsub!("o","O")
-puts "Your new name is #{name}"
-
