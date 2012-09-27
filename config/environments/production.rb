06b1d95fb7d063ab5212a9712937d16214a6fedf
Robert Forillo <infocord2000@yahoo.com>
2012-09-26 18:47:05 -0700

    production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,21 +1,48 @@
 Railinstaller::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
+  #
+  #
   # Code is not reloaded between requests
   config.cache_classes = true
+
+  # Log error messages when you accidentally call methods on nil.
+  # config.whiny_nils = true
 
   # Full error reports are disabled and caching is turned on
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
-  # Disable Rails's static asset server (Apache or nginx will already do this)
-  config.serve_static_assets = false
+  # Disable delivery errors, bad email addresses will be ignored
+  #config.action_mailer.raise_delivery_errors = false
+
+  # Send deprecation notices to registered listeners
+  config.active_support.deprecation = :notify
+  
+
+  # Only use best-standards-support built into browsers
+  # config.action_dispatch.best_standards_support = :builtin
+
+  # Raise exception on mass assignment protection for Active Record models
+  # config.active_record.mass_assignment_sanitizer = :strict
+
+  # Log the query plan for queries taking more than this (works
+  # with SQLite, MySQL, and PostgreSQL)
+  # config.active_record.auto_explain_threshold_in_seconds = 0.5
 
   # Compress JavaScripts and CSS
   config.assets.compress = true
 
+  # Expands the lines which load the assets
+  # config.assets.debug = true
+
+
+
+  # Disable Rails's static asset server (Apache or nginx will already do this)
+  config.serve_static_assets = false
+
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  # config.assets.compile = false
 
   # Generate digests for assets URLs
   config.assets.digest = true
@@ -48,9 +75,6 @@ Railinstaller::Application.configure do
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
 
-  # Disable delivery errors, bad email addresses will be ignored
-  # config.action_mailer.raise_delivery_errors = false
-
   # Enable threaded mode
   # config.threadsafe!
 
@@ -58,10 +82,4 @@ Railinstaller::Application.configure do
   # the I18n.default_locale when a translation can not be found)
   config.i18n.fallbacks = true
 
-  # Send deprecation notices to registered listeners
-  config.active_support.deprecation = :notify
-
-  # Log the query plan for queries taking more than this (works
-  # with SQLite, MySQL, and PostgreSQL)
-  # config.active_record.auto_explain_threshold_in_seconds = 0.5
 end
