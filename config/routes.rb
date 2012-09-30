2d1bbbcbf22f06d15d7049106514faacc9589c87
Robert Forillo <infocord2000@yahoo.com>
2012-09-29 17:54:41 -0700

    routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,6 @@ Railinstaller::Application.routes.draw do
 
   # Sample resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
-  resources :wordpress
 
   # Sample resource route with options:
   #   resources :products do
